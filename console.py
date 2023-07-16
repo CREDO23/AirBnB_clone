a0248ef5b7af833541e3b4a499d9e7c072becf0f
CREDO23 <thierrybakera12@gmail.com>
2023-07-16 15:20:25 +0200

    add requiremetns

--- console.py
+++ console.py
@@ -7,37 +7,34 @@ import re
 from shlex import split
 from models import storage
 from models.base_model import BaseModel
-from models.user import User
-from models.state import State
 from models.city import City
 from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
+from models.user import User
+from models.state import State
 
 
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
+def parse(line):
+    cl_b = re.search(r"\{(.*?)\}", line)
+    bc = re.search(r"\[(.*?)\]", line)
+    if cl_b is None:
+        if bc is None:
+            return [i.strip(",") for i in split(line)]
         else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
+            l_x_r = split(line[:bc.span()[0]])
+            r_t_l = [i.strip(",") for i in l_x_r]
+            r_t_l.append(bc.group())
+            return r_t_l
     else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
+        l_x_r = split(line[:cl_b.span()[0]])
+        r_t_l = [i.strip(",") for i in l_x_r]
+        r_t_l.append(cl_b.group())
+        return r_t_l
 
 
 class HBNBCommand(cmd.Cmd):
-    """Defines the HolbertonBnB command interpreter.
-    Attributes:
-        prompt (str): The command prompt.
-    """
+    """The command interpreter"""
 
     prompt = "(hbnb) "
     __classes = {
@@ -54,7 +51,7 @@ class HBNBCommand(cmd.Cmd):
         """Do nothing upon receiving an empty line."""
         pass
 
-    def default(self, arg):
+    def default(self, line):
         """Default behavior for cmd module when input is invalid"""
         argdict = {
             "all": self.do_all,
@@ -63,23 +60,23 @@ class HBNBCommand(cmd.Cmd):
             "count": self.do_count,
             "update": self.do_update
         }
-        match = re.search(r"\.", arg)
+        match = re.search(r"\.", line)
         if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
+            argl = [line[:match.span()[0]], line[match.span()[1]:]]
             match = re.search(r"\((.*?)\)", argl[1])
             if match is not None:
                 command = [argl[1][:match.span()[0]], match.group()[1:-1]]
                 if command[0] in argdict.keys():
                     call = "{} {}".format(argl[0], command[1])
                     return argdict[command[0]](call)
-        print("*** Unknown syntax: {}".format(arg))
+        print("*** Unknown syntax: {}".format(line))
         return False
 
-    def do_quit(self, arg):
+    def do_quit(self, line):
         """Quit command to exit the program."""
         return True
 
-    def do_EOF(self, arg):
+    def do_EOF(self, line):
         """EOF signal to exit the program."""
         print("")
         return True
@@ -118,11 +115,11 @@ class HBNBCommand(cmd.Cmd):
         except NameError:
             print("** class doesn't exist **")
 
-    def do_show(self, arg):
+    def do_show(self, line):
         """Usage: show <class> <id> or <class>.show(<id>)
         Display the string representation of a class instance of a given id.
         """
-        argl = parse(arg)
+        argl = parse(line)
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
@@ -135,10 +132,10 @@ class HBNBCommand(cmd.Cmd):
         else:
             print(objdict["{}.{}".format(argl[0], argl[1])])
 
-    def do_destroy(self, arg):
+    def do_destroy(self, line):
         """Usage: destroy <class> <id> or <class>.destroy(<id>)
         Delete a class instance of a given id."""
-        argl = parse(arg)
+        argl = parse(line)
         objdict = storage.all()
         if len(argl) == 0:
             print("** class name missing **")
@@ -152,11 +149,11 @@ class HBNBCommand(cmd.Cmd):
             del objdict["{}.{}".format(argl[0], argl[1])]
             storage.save()
 
-    def do_all(self, arg):
+    def do_all(self, line):
         """Usage: all or all <class> or <class>.all()
         Display string representations of all instances of a given class.
         If no class is specified, displays all instantiated objects."""
-        argl = parse(arg)
+        argl = parse(line)
         if len(argl) > 0 and argl[0] not in HBNBCommand.__classes:
             print("** class doesn't exist **")
         else:
@@ -168,23 +165,23 @@ class HBNBCommand(cmd.Cmd):
                     objl.append(obj.__str__())
             print(objl)
 
-    def do_count(self, arg):
+    def do_count(self, line):
         """Usage: count <class> or <class>.count()
         Retrieve the number of instances of a given class."""
-        argl = parse(arg)
+        argl = parse(line)
         count = 0
         for obj in storage.all().values():
             if argl[0] == obj.__class__.__name__:
                 count += 1
         print(count)
 
-    def do_update(self, arg):
+    def do_update(self, line):
         """Usage: update <class> <id> <attribute_name> <attribute_value> or
        <class>.update(<id>, <attribute_name>, <attribute_value>) or
        <class>.update(<id>, <dictionary>)
         Update a class instance of a given id by adding or updating
         a given attribute key/value pair or dictionary."""
-        argl = parse(arg)
+        argl = parse(line)
         objdict = storage.all()
 
         if len(argl) == 0:
